4d13394e0bdc4a3d1d28c2daae681fc3517c8dbe
nmhung <nmhung@cmc.com.vn>
2021-05-30 20:25:28 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { OrganizationComponent } from './organization/organization.component';
+import { LoginComponent } from './core/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'organization', component: OrganizationComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
